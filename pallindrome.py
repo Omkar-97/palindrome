319ae8e711f5a9369cba07863d14a27b677acf15
Omkar Sreekant Pathak <omkar.pathak77@gmail.com>
2019-08-10 14:24:20 +0530

    Add files via upload

---
+++ pallindrome.py
@@ -0,0 +1,31 @@
+"""
+Input: string
+Output: List of palindrome occurrences and length of palindrome list
+"""
+
+
+def get_palindrome(string):
+    cnt = 1
+    palindrome_occr = []
+    string_len = len(string)
+
+    while string_len != 0:
+        for occr in range(0, string_len):
+            temp = string[occr:occr+cnt]
+            if temp not in palindrome_occr:
+                if str(temp) == str(temp)[::-1]:
+                    palindrome_occr.append(temp)
+        cnt += 1
+        string_len -= 1
+    return len(palindrome_occr), palindrome_occr
+
+
+if __name__ == '__main__':
+    occr_cnt = 1
+    input_string = input()
+    pall_len, pall_list = get_palindrome(input_string)
+    print("{} palindrome occurrences".format(pall_len))
+    for val in pall_list:
+        print("Occurrence {}: {}".format(occr_cnt, val))
+        occr_cnt += 1
+
